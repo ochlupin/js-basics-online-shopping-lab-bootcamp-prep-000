ef24e73c835177e2248cceb9e5980371eb5301ac
Vu Phong Nguyen <vuphongnguyen87@gmail.com>
2018-04-30 18:20:06 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -43,7 +43,7 @@ var total=0
 function removeFromCart(item) {
 for (let i=0;i<cart.length;i++){
   if (cart[i].hasOwnProperty(item)){
-    return cart.splice(i,1):
+    return cart.splice(i,1);
   }
 }
 return ("That item is not in your cart.")
