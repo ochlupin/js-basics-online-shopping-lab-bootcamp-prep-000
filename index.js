8dc187d2c420e5ac7911d6f1d2213940a0074917
Vu Phong Nguyen <vuphongnguyen87@gmail.com>
2018-04-30 16:12:23 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ function setCart(c) {
 }
 
 function addToCart(item) {
- var newItem{itemName:item,itemPrice:Math.floor(Math.random()*Math.floor(100))}
+ var newItem={itemName:item,itemPrice:Math.floor(Math.random()*Math.floor(100))}
  cart.push(newItem);
  console.log(`${item} has been added to your cart.`);
  return cart;
