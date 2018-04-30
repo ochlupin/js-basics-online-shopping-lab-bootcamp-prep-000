8a9bf8908d3d4089da1c366b223ad94749204bce
Vu Phong Nguyen <vuphongnguyen87@gmail.com>
2018-04-30 18:02:02 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -41,7 +41,14 @@ var total=0
 }
 
 function removeFromCart(item) {
-  // write your code here
+for (let i=0;i<cart.length;i++){
+  if (cart[i].hasOwnProperty(item)===true){
+    cart.splice(i,1)
+    return cart
+  } else{
+    console.log(`That item is not in your cart.`)
+  }
+}
 }
 
 function placeOrder(cardNumber) {
